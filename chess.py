94f3b0a64947538b5ce1469c8d07e14a1a78e13c
Nathan Dunn <nathanfdunn@gmail.com>
2017-04-26 20:24:54 -0400

    Made board parsing more tolerant and added reset option

--- chess.py
+++ chess.py
@@ -7,37 +7,47 @@ import sys
 import os
 here = pathlib.Path(__file__).parent
 
-# Invert colors
-os.system(r"printf '\e[7m'")
-
-board = []
-with open(here / 'chessboard.txt') as board_file:
-    for line in board_file:
-        board.append(list(line.strip('\n')))
+def get_board():
+    board = []
+    with open(here / '.chessboard') as board_file:
+        for i in range(8):
+            line = next(board_file, '')
+            line = line.strip('\n').ljust(8)
+            board.append(list(line))
+        print(board_file.read())
+    return board
 
 def print_board(board):
+    # Invert colors
+    os.system(r"printf '\e[7m'")
     piece_map = dict(zip('kqrbnpKQRBNP', '♔♕♖♗♘♙♚♛♜♝♞♟'))
 
     pretty_board = [[None]*8 for i in range(8)]
     for i in range(8):
         for j in range(8):
-            if board[i][j] not in piece_map:
-                square = '::' if (i+j) % 2 else '  '
-            else:
+            if board[i][j] in piece_map:
                 square = piece_map[ board[i][j] ] + ' '
+            else:
+                square = '::' if (i+j) % 2 else '  '
             pretty_board[i][j] = square
 
-    char = '#'
-    final = 20*char + '\n'
+    border = '#'
+    final = 20*border + '\n'
     for line in pretty_board:
-        final += char + ' ' + ''.join(line) + ' ' + char + '\n'
-    final += 20*char + '\n'
+        final += border + ' ' + ''.join(line) + ' ' + border + '\n'
+    final += 20*border + '\n'
     print(final)
+    # Revert colors
+    os.system(r"printf '\e[0m'")
 
 def write_board(board):
-    with open(here / 'chessboard.txt', 'w') as board_file:
+    with open(here / '.chessboard', 'w') as board_file:
         for line in board:
             print(''.join(line), file=board_file)
+
+def reset_board():
+    with open(here / '.chessboard', 'w') as board_file:
+        board_file.write('RNBQKBNR\nPPPPPPPP\n\n\n\n\npppppppp\nrnbqkbnr')
 
 # Converts to the y, x coords so you can do board[y][x]
 # 'C3' -> 5, 2
@@ -47,11 +57,25 @@ def convert_coord(alg_notation):
     y = 8 - int(rank)
     return y, x
 
+
+
+
+
+
+
+
 # TODO make an ArgParser
 if len(sys.argv) == 1:
-    print_board(board)
+    print_board(get_board())
+
+elif len(sys.argv) == 2:
+    if sys.argv[1].lower() == 'reset':
+        reset_board()
+    else:
+        print('Bad options')
 
 elif len(sys.argv) == 3:
+    board = get_board()
     start, end = sys.argv[1:]
     s_y, s_x = convert_coord(start)
     e_y, e_x = convert_coord(end)
@@ -60,12 +84,7 @@ elif len(sys.argv) == 3:
     write_board(board)
     print_board(board)
 
-elif len(sys.argv) == 2:
-    # Assume it's piece creation
-    ...
-
 else:
     print('Bad options')
 
-# Revert colors
-os.system(r"printf '\e[0m'")
+
