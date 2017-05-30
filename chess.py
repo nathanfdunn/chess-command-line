9f3db1b9e25b0bbf9819c51a4f7390e5960f8289
Nathan Dunn <nathanfdunn@gmail.com>
2017-05-30 17:49:39 -0400

    Better border, added instructions, made presentable for GitHub

--- chess.py
+++ chess.py
@@ -1,31 +1,34 @@
 #!/usr/bin/env python3
-# Copy this content to a file named 'chess', put it somewhere on your PATH and run `chmod +x chess`
-# Put the '.chessboard' file in the same folder as the 'chess' file
+# Copy this content to a file named `chess`, put it somewhere on your PATH and run `chmod +x chess`
 
 import pathlib
 import sys
 import os
-here = pathlib.Path(__file__).parent
+import subprocess
+board_path = pathlib.Path(__file__).parent / '.chessboard'
 
 def get_board():
     board = []
-    with open(here / '.chessboard') as board_file:
+    if not os.path.exists(board_path):
+        reset_board()
+
+    with open(board_path) as board_file:
         for i in range(8):
             line = next(board_file, '')
             line = line.strip('\n').ljust(8)
             board.append(list(line))
-        print(board_file.read())
+
     return board
 
 def square_string(piece, square_is_white):
     out = ''
     if piece.lower() in 'kqrbnp':
-        piece_is_white = piece in 'KQRBNP'.lower()      #yeah, yeah. We'll be FEN compliant soon
+        piece_is_white = piece in 'KQRBNP'
         if piece_is_white == square_is_white:
             # We need the outline version
             piece_char = dict(zip('kqrbnp', '♔♕♖♗♘♙'))[piece.lower()] + ' '
         else:
-            # We can use the solid version
+            # We need the solid version
             piece_char = dict(zip('kqrbnp', '♚♛♜♝♞♟'))[piece.lower()] + ' '
     else:
         piece_char = '  '
@@ -38,30 +41,31 @@ def square_string(piece, square_is_white):
     out += piece_char
     return out
 
-def print_board(board, border=False):
+def print_board(board, border=True):
     string = r"printf '\e[0m"
     if border:
-        string += r'<>================<>\n'
+        string += r'╔════════════════╗\n'
     for i, row in enumerate(board):
-        string += r'\e[0m' + ('||' if border else '')
+        string += r'\e[0m' + ('║' if border else '')
         for j, piece in enumerate(row):
             string += square_string(board[i][j], (i+j) % 2)
-        string += r'\e[0m' + ('||' if border else '') + r'\n'
+        string += r'\e[0m' + ('║' if border else '') + r'\n'
     if border:
-        string += '<>================<>\n'
-    string += "'"
-    with open('print-chessboard.sh', 'w') as f:
-        f.write(string)
-    os.system(string)
+        string += r'╚════════════════╝\n'
+    string += r"\n'"
+    # with open('print-chessboard.sh', 'w') as f:
+    #     f.write(string)
+    # os.system(string)
+    subprocess.call(['bash', '-c', string])
 
 def write_board(board):
-    with open(here / '.chessboard', 'w') as board_file:
+    with open(board_path, 'w') as board_file:
         for line in board:
             print(''.join(line), file=board_file)
 
 def reset_board():
-    with open(here / '.chessboard', 'w') as board_file:
-        board_file.write('RNBQKBNR\nPPPPPPPP\n\n\n\n\npppppppp\nrnbqkbnr')
+    with open(board_path, 'w') as board_file:
+        board_file.write('rnbqkbnr\npppppppp\n\n\n\n\nPPPPPPPP\nRNBQKBNR')
 
 # Converts to the y, x coords so you can do board[y][x]
 # 'C3' -> 5, 2
@@ -78,6 +82,7 @@ if len(sys.argv) == 1:
 elif len(sys.argv) == 2:
     if sys.argv[1].lower() == 'reset':
         reset_board()
+        print_board(get_board())
     else:
         print('Bad options')
 
---
+++ readme.md
@@ -0,0 +1,9 @@
+# ????
+![example](usage.png)
+
+# Instructions
+Copy the chess.py file somewhere on your PATH, rename the file to `chess`, and run `chmod +x chess`
+
+Display looks best with character spacing = 1.034, line spacing = 0.819, size 18 Monaco font.
+
+![font settings](font-settings.png)
